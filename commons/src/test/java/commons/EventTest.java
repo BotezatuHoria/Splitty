bfafd3e82edec293f2e83bb5e381a9a0394eb225
Tim Guldenmundt <t.m.j.guldenmundt@student.tudelft.nl>
2024-02-25 14:37:38 +0100

    Added javadoc to all the tests

--- commons/src/test/java/commons/EventTest.java
+++ commons/src/test/java/commons/EventTest.java
@@ -3,37 +3,54 @@ package commons;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class EventTest {
-
+    /**
+     * Test the getTag method
+     */
     @Test
     void getTagTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
         assertEquals("Event one", test.getTag());
     }
 
+    /**
+     * Test the getTitle method
+     */
     @Test
     void getTitleTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
         assertEquals("Party", test.getTitle());
     }
 
+    /**
+     * Test the getId method
+     */
     @Test
     void getIdTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
         assertEquals(1, test.getId());
     }
 
+    /**
+     * Test the getToken method
+     */
     @Test
     void getTokenTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
         assertEquals("1234", test.getToken());
     }
 
+    /**
+     * Test the toString method
+     */
     @Test
     void toStringTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
         assertEquals("Event{tag='Event one', title='Party', id=1, token='1234'}", test.toString());
     }
 
+    /**
+     * Test the equals method with two similar events
+     */
     @Test
     void equalsTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
@@ -41,6 +58,9 @@ class EventTest {
         assertEquals(test, test2);
     }
 
+    /**
+     * Test equals method with two events with different tags
+     */
     @Test
     void DifferentTagTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
@@ -48,6 +68,9 @@ class EventTest {
         assertNotEquals(test, test2);
     }
 
+    /**
+     * Test equals method with two events with different titles
+     */
     @Test
     void DifferentTitleTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
@@ -55,6 +78,9 @@ class EventTest {
         assertNotEquals(test, test2);
     }
 
+    /**
+     * Test equals method with two events with different id's
+     */
     @Test
     void DifferentIdTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
@@ -62,13 +88,19 @@ class EventTest {
         assertNotEquals(test, test2);
     }
 
+    /**
+     * Test equals method with two events with different tokens
+     */
     @Test
     void DifferentTokenTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
-        Event test2 = new Event("Event one", "Party", 1, "ABCD");
+        Event test2 = new Event("Event one", "Party", 1, "4321");
         assertNotEquals(test, test2);
     }
 
+    /**
+     * Test hashcode method with two similar events
+     */
     @Test
     void HashcodeTest(){
         Event test = new Event("Event one", "Party", 1, "1234");
