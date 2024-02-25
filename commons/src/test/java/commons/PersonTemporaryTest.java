3f9d8a540001575b86828ef2014add3e111a0bd3
Ilia Ilinsky <I.S.Ilinsky-1@student.tudelft.nl>
2024-02-25 13:29:33 +0100

    Initial commit with tests for person

---
+++ commons/src/test/java/commons/PersonTemporaryTest.java
@@ -0,0 +1,44 @@
+package commons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTemporaryTest {
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void getIban() {
+    }
+
+    @Test
+    void getEventID() {
+    }
+
+    @Test
+    void getDebt() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void addDept() {
+    }
+}
