b5648a41e0c4c90698f77dd8570bd5f571312aab
Horia Botezatu <h.a.botezatu@student.tudelft.nl>
2024-02-25 11:32:25 +0100

    Further tests added for the TransactionTest class

--- commons/src/test/java/commons/TransactionTest.java
+++ commons/src/test/java/commons/TransactionTest.java
@@ -1,6 +1,7 @@
 package commons;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +19,61 @@ public class TransactionTest {
         assertEquals(10, t.getDate().getDayOfMonth());
         assertEquals(100, t.getMoney());
         assertEquals(947, t.getCurrency());
+    }
+
+    @Test
+    public void testName() {
+        Transaction t = new Transaction("test",
+                LocalDate.of(Integer.parseInt("1970"), Integer.parseInt("10"), Integer.parseInt("10")),
+                100, 947);
+        assertEquals("test", t.getName());
+    }
+    @Test
+    public void testLocalDate() {
+        Transaction t = new Transaction("test",
+                LocalDate.of(Integer.parseInt("1970"), Integer.parseInt("10"), Integer.parseInt("10")),
+                100, 947);
+        assertEquals(1970, t.getDate().getYear());
+        assertEquals(1970, t.getDate().getYear());
+        assertEquals(10, t.getDate().getMonthValue());
+        assertEquals(10, t.getDate().getDayOfMonth());
+    }
+
+    @Test
+    public void testMoney() {
+        Transaction t = new Transaction("test",
+                LocalDate.of(Integer.parseInt("1970"), Integer.parseInt("10"), Integer.parseInt("10")),
+                100, 947);
+        assertEquals(100, t.getMoney());
+    }
+
+    @Test
+    public void testCurrency() {
+        Transaction t = new Transaction("test",
+                LocalDate.of(Integer.parseInt("1970"), Integer.parseInt("10"), Integer.parseInt("10")),
+                100, 947);
+        assertEquals(947, t.getCurrency());
+    }
+
+    @Test
+    public void testEquals() {
+        Transaction t1 = new Transaction("test",
+                LocalDate.of(Integer.parseInt("1970"), Integer.parseInt("10"), Integer.parseInt("10")),
+                100, 947);
+        Transaction t2 = new Transaction("test",
+                LocalDate.of(Integer.parseInt("1970"), Integer.parseInt("10"), Integer.parseInt("10")),
+                100, 947);
+        assertEquals(t1, t2);
+    }
+
+    @Test
+    public void testNotEquals() {
+        Transaction t1 = new Transaction("test",
+                LocalDate.of(Integer.parseInt("1970"), Integer.parseInt("10"), Integer.parseInt("10")),
+                100, 947);
+        Transaction t2 = new Transaction("testing",
+                LocalDate.of(Integer.parseInt("1971"), Integer.parseInt("2"), Integer.parseInt("5")),
+                10, 840);
+        assertNotEquals(t1, t2);
     }
 }
