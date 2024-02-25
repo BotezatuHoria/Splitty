180ae635047c114847203e60b0b7ea20c019b6c8
Tim Guldenmundt <t.m.j.guldenmundt@student.tudelft.nl>
2024-02-25 14:30:44 +0100

    Added more test for the Event class

--- commons/src/test/java/commons/EventTest.java
+++ commons/src/test/java/commons/EventTest.java
@@ -27,4 +27,52 @@ class EventTest {
         Event test = new Event("Event one", "Party", 1, "1234");
         assertEquals("1234", test.getToken());
     }
+
+    @Test
+    void toStringTest(){
+        Event test = new Event("Event one", "Party", 1, "1234");
+        assertEquals("Event{tag='Event one', title='Party', id=1, token='1234'}", test.toString());
+    }
+
+    @Test
+    void equalsTest(){
+        Event test = new Event("Event one", "Party", 1, "1234");
+        Event test2 = new Event("Event one", "Party", 1, "1234");
+        assertEquals(test, test2);
+    }
+
+    @Test
+    void DifferentTagTest(){
+        Event test = new Event("Event one", "Party", 1, "1234");
+        Event test2 = new Event("Event two", "Party", 1, "1234");
+        assertNotEquals(test, test2);
+    }
+
+    @Test
+    void DifferentTitleTest(){
+        Event test = new Event("Event one", "Party", 1, "1234");
+        Event test2 = new Event("Event one", "BBQ", 1, "1234");
+        assertNotEquals(test, test2);
+    }
+
+    @Test
+    void DifferentIdTest(){
+        Event test = new Event("Event one", "Party", 1, "1234");
+        Event test2 = new Event("Event one", "Party", 2, "1234");
+        assertNotEquals(test, test2);
+    }
+
+    @Test
+    void DifferentTokenTest(){
+        Event test = new Event("Event one", "Party", 1, "1234");
+        Event test2 = new Event("Event one", "Party", 1, "ABCD");
+        assertNotEquals(test, test2);
+    }
+
+    @Test
+    void HashcodeTest(){
+        Event test = new Event("Event one", "Party", 1, "1234");
+        Event test2 = new Event("Event one", "Party", 1, "1234");
+        assertEquals(test.hashCode(), test2.hashCode());
+    }
 }
