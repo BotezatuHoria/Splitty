3fee3db24bf7e1ee73701dd5024d0784362ec2c0
Tom van Leest <t.p.p.vanleest@student.tudelft.nl>
2024-02-29 16:58:06 +0100

    The fxml page of Sending and invite with the corresponding control page, functionality is still to be added. Checkstyle checked.

--- client/src/main/java/client/scenes/InviteSendingCtrl.java
+++ client/src/main/java/client/scenes/InviteSendingCtrl.java
@@ -4,12 +4,21 @@
 
 package client.scenes;
 
+import client.utils.ServerUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
-public class InviteSendingCtrl {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class InviteSendingCtrl{
+
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
 
     @FXML // fx:id="CopyInviteCodeButton"
     private Button CopyInviteCodeButton; // Value injected by FXMLLoader
@@ -25,6 +34,32 @@ public class InviteSendingCtrl {
 
     @FXML // fx:id="SendInviteButton"
     private Button SendInviteButton; // Value injected by FXMLLoader
+
+    /**
+     *very great thing i need to finish.
+     * @param server the server to connect with
+     * @param mainCtrl
+     */
+    public InviteSendingCtrl(ServerUtils server, MainCtrl mainCtrl) {
+        this.server = server;
+        this.mainCtrl = mainCtrl;
+    }
+
+    public void sendInvite() {
+        String mails = MailInputField.getText();
+        Scanner scanner = new Scanner(mails);
+        List<String> listOfMails = new ArrayList<>();
+        while (scanner.hasNext()){
+            listOfMails.add(scanner.next());
+        }
+        for(String s:listOfMails){
+            String recepient = s;
+            String sender = "t.p.p.vanLeest@student.tudelft.nl";
+        }
+
+
+    }
+
 
 
 }
