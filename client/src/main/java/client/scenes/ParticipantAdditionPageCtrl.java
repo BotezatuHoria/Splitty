3d402dbc2f1b147d71f4cfc6ba7e9d4443d81709
Tim Guldenmundt <t.m.j.guldenmundt@student.tudelft.nl>
2024-02-29 12:04:01 +0100

    Created fxml file and controller file for the participants addition page

---
+++ client/src/main/java/client/scenes/ParticipantAdditionPageCtrl.java
@@ -0,0 +1,37 @@
+package client.scenes;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import client.utils.ServerUtils;
+import commons.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
+
+public class ParticipantAdditionPageCtrl {
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private Button abortButton;
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField iban;
+
+
+}
