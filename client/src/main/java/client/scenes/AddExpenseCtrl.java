e7e7fe15dcf4c47c76db22073f8c3c022e70c242
Horia Botezatu <h.a.botezatu@student.tudelft.nl>
2024-02-29 14:00:02 +0100

    Redesign ok the Add Expense Page

--- client/src/main/java/client/scenes/AddExpenseCtrl.java
+++ client/src/main/java/client/scenes/AddExpenseCtrl.java
@@ -6,7 +6,6 @@ package client.scenes;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
@@ -20,14 +19,14 @@ public class AddExpenseCtrl {
     @FXML // fx:id="addButton"
     private Button addButton; // Value injected by FXMLLoader
 
+    @FXML // fx:id="addButton1"
+    private Button addButton1; // Value injected by FXMLLoader
+
     @FXML // fx:id="currencyBox"
     private ComboBox<?> currencyBox; // Value injected by FXMLLoader
 
     @FXML // fx:id="dateBox"
     private DatePicker dateBox; // Value injected by FXMLLoader
-
-    @FXML // fx:id="equalCheck"
-    private CheckBox equalCheck; // Value injected by FXMLLoader
 
     @FXML // fx:id="expenseLabel"
     private TextField expenseLabel; // Value injected by FXMLLoader
@@ -43,8 +42,5 @@ public class AddExpenseCtrl {
 
     @FXML // fx:id="priceLabel"
     private TextField priceLabel; // Value injected by FXMLLoader
-
-    @FXML // fx:id="someCheck"
-    private CheckBox someCheck; // Value injected by FXMLLoader
 
 }
