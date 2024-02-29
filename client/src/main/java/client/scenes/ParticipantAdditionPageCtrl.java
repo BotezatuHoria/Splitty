36e006de5df031b6aa8bdcc8dde146e9e0267369
Tim Guldenmundt <t.m.j.guldenmundt@student.tudelft.nl>
2024-02-29 12:09:11 +0100

    added basic controller for participant addition page, generated by SceneBuilder

--- client/src/main/java/client/scenes/ParticipantAdditionPageCtrl.java
+++ client/src/main/java/client/scenes/ParticipantAdditionPageCtrl.java
@@ -1,37 +1,50 @@
+/**
+ * Sample Skeleton for 'ParticipantAdditionPage.fxml' Controller Class
+ */
+
 package client.scenes;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
-
-import client.utils.ServerUtils;
-import commons.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import org.w3c.dom.Text;
 
 public class ParticipantAdditionPageCtrl {
 
-    @FXML
-    private Button createButton;
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
 
-    @FXML
-    private Button abortButton;
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
 
-    @FXML
-    private TextField firstName;
+    @FXML // fx:id="abortButton"
+    private Button abortButton; // Value injected by FXMLLoader
 
-    @FXML
-    private TextField lastName;
+    @FXML // fx:id="createButton"
+    private Button createButton; // Value injected by FXMLLoader
 
-    @FXML
-    private TextField email;
+    @FXML // fx:id="email"
+    private TextField email; // Value injected by FXMLLoader
 
-    @FXML
-    private TextField iban;
+    @FXML // fx:id="firstName"
+    private TextField firstName; // Value injected by FXMLLoader
 
+    @FXML // fx:id="iban"
+    private TextField iban; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lastName"
+    private TextField lastName; // Value injected by FXMLLoader
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert abortButton != null : "fx:id=\"abortButton\" was not injected: check your FXML file 'ParticipantAdditionPage.fxml'.";
+        assert createButton != null : "fx:id=\"createButton\" was not injected: check your FXML file 'ParticipantAdditionPage.fxml'.";
+        assert email != null : "fx:id=\"email\" was not injected: check your FXML file 'ParticipantAdditionPage.fxml'.";
+        assert firstName != null : "fx:id=\"firstName\" was not injected: check your FXML file 'ParticipantAdditionPage.fxml'.";
+        assert iban != null : "fx:id=\"iban\" was not injected: check your FXML file 'ParticipantAdditionPage.fxml'.";
+        assert lastName != null : "fx:id=\"lastName\" was not injected: check your FXML file 'ParticipantAdditionPage.fxml'.";
+
+    }
 
 }
