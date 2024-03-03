c03c4f662b5bda05d724954043607a0d9519e7f3
Tautvydas Kuklys <T.Kuklys@student.tudelft.nl>
2024-03-03 20:39:53 +0100

    removing unused imports

--- server/src/main/java/server/database/PersonRepository.java
+++ server/src/main/java/server/database/PersonRepository.java
@@ -2,7 +2,5 @@ package server.database;
 
 import commons.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface PersonRepository extends JpaRepository<Person, String> {}
