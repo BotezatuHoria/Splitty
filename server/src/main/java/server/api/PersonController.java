3670020a1d77e18d7935c093d1e5cf1ad29c4356
Tautvydas Kuklys <T.Kuklys@student.tudelft.nl>
2024-03-03 20:08:24 +0100

    Adding person API endpoint

---
+++ server/src/main/java/server/api/PersonController.java
@@ -0,0 +1,87 @@
+package server.api;
+
+import commons.Person;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.database.PersonRepository;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+@RestController
+@RequestMapping("/api/person")
+public class PersonController {
+    private final PersonRepository db;
+
+    /**
+     * Constructor for the PersonController
+     * @param db repository for the personRepository
+     */
+    public PersonController(PersonRepository db) {
+        this.db = db;
+    }
+
+    /**
+     * Get all people as json
+     * @return list of people as json
+     */
+    @GetMapping(path = { "", "/" })
+    public List<Person> getAll() {
+        return db.findAll();
+    }
+
+    /**
+     * Gets person entity by supplied email
+     * @param email the email of the user
+     * @return person who`s email matches supplied email
+     */
+    @GetMapping("/{email}")
+    public ResponseEntity<Person> getByEmail(@PathVariable("email") String email) {
+        if (!db.existsById(email)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(db.findById(email).get());
+    }
+
+    /**
+     * Adds person to the database
+     * @param person the person to add
+     * @return person that was added
+     */
+    @PostMapping(path = { "", "/" })
+    public ResponseEntity<Person> addPerson(Person person) {
+        // The regex ensures that:
+        //  - email begins with alphanumeric characters, including underscores, pluses, ampersands, asterisks, or hyphens.
+        //  - may contain dots as long as they are not the first or last character and it does not come one after the other.
+        //  - contains an @ symbol separating the local part from the domain part.
+        //  - the domain part contains alphanumeric characters, including hyphens, but again, hyphens cannot be the first character. It then has a period . and a domain name with 2 to 7 letters to comply with domain name restrictions.
+
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern emailPattern = Pattern.compile(emailRegex);
+
+        if (person.getEmail() == null || !emailPattern.matcher(person.getEmail()).matches()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Person saved = db.save(person);
+        return ResponseEntity.ok(saved);
+    }
+
+    /**
+     * Deletes person by their email
+     * @param email the email to delete by
+     * @return person that was deleted
+     */
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Person> deletePerson(@PathVariable("email") String email) {
+        if (!db.existsById(email)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        ResponseEntity<Person> person = ResponseEntity.ok(db.findById(email).get());
+        db.deleteById(email);
+        return person;
+    }
+
+}
---
+++ server/src/main/java/server/database/PersonRepository.java
@@ -0,0 +1,8 @@
+package server.database;
+
+import commons.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PersonRepository extends JpaRepository<Person, String> {}
