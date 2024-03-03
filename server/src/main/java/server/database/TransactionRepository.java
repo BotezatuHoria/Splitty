cb35b3ba9518e7866ce30ef4e9f0779c089c6b24
Tim Guldenmundt <t.m.j.guldenmundt@student.tudelft.nl>
2024-03-03 22:28:21 +0100

    Added a basic api for the transaction class

---
+++ server/src/main/java/server/database/TransactionRepository.java
@@ -0,0 +1,8 @@
+package server.database;
+
+import commons.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+
+}
