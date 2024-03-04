049140554b404a4339379f44063ff0e2e011b73a
Horia Botezatu <h.a.botezatu@student.tudelft.nl>
2024-03-04 12:09:22 +0100

    Connected the event page with the other pages + Missing javadoc

--- client/src/main/java/client/scenes/LanguageSelectorCtrl.java
+++ client/src/main/java/client/scenes/LanguageSelectorCtrl.java
@@ -14,17 +14,25 @@ public class LanguageSelectorCtrl {
     @FXML
     private Button backButton;
 
+    /**
+     * Constructor for the language selector controller.
+     * @param mainCtrl - reference to the main controller
+     */
     @Inject
     public LanguageSelectorCtrl(MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
     }
+
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert languageComboBox != null : "fx:id=\"languageComboBox\" was not injected: check your FXML file 'AddExpense.fxml'.";
     }
 
-    public void goBack() {
+    /**
+     * Method that goes back to the home page.
+     */
+    public void goHome() {
         mainCtrl.showStarter();
     }
 }
