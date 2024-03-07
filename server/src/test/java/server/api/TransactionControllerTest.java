669fa8b0c4cf4158c870a16eb8ace148cd5ca746
Tom van Leest <t.p.p.vanleest@student.tudelft.nl>
2024-03-07 12:59:02 +0100

    Expanded the transaction api to (hopefully) update transactions and their attributes. Part of functionality will be commited and merged by Artur. Test will come.

---
+++ server/src/test/java/server/api/TransactionControllerTest.java
@@ -0,0 +1,36 @@
+package server.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void updateNameById() {
+    }
+
+    @Test
+    void updateParticipantsById() {
+    }
+
+    @Test
+    void updateCreatorById() {
+    }
+}
