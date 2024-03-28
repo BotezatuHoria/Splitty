fbbe7b8e3acf80feb2a4fbbc658acf41285b2ff2
Tim Guldenmundt <t.m.j.guldenmundt@student.tudelft.nl>
2024-03-28 11:27:53 +0100

    Added a listview on the admin page that shows all events from the database

--- client/src/main/java/client/scenes/AdminPageCtrl.java
+++ client/src/main/java/client/scenes/AdminPageCtrl.java
@@ -1,12 +1,43 @@
 package client.scenes;
 
 import client.utils.ServerUtils;
+import commons.Event;
 import jakarta.inject.Inject;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.Date;
 
 public class AdminPageCtrl {
 
     private final MainCtrl mainCtrl;
-    private final ServerUtils serverUtils;
+    private final ServerUtils server;
+
+
+    @FXML
+    private TableView<Event> events;
+
+    @FXML
+    private TableColumn<Event, String> titleColumn;
+    @FXML
+    private TableColumn<Event, Date> creationDateColumn;
+    @FXML
+    private TableColumn<Event, Date> lastModifiedColumn;
+
+    @FXML
+    private Button deleteEvent;
+
+    @FXML
+    private Button downloadEvent;
+
+    @FXML
+    private Button importEvent;
+
+    @FXML
+    private Label selectedEvent;
 
     /**
      * Constructor for the admin page controller.
@@ -15,11 +46,28 @@ public class AdminPageCtrl {
      */
     @Inject
     public AdminPageCtrl(ServerUtils server, MainCtrl mainCtrl){
-        this.serverUtils = server;
+        this.server = server;
         this.mainCtrl = mainCtrl;
+    }
+
+    /**
+     * Gets all the events from the server and adds them to the tableview.
+     */
+    public void showEvents(){
+        ObservableList<Event> items = FXCollections.observableArrayList();
+        items.addAll(server.getEvents());
+        events.setItems(items);
+    }
+    @FXML
+    void initialize(){
+        //set up the columns in the table
+        titleColumn.setCellValueFactory(new PropertyValueFactory<Event, String>("title"));
+        showEvents();
     }
 
     public void goBack() {
         mainCtrl.showAdminLogin();
     }
+
+
 }
