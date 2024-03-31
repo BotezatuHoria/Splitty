65a12cc3b01427630225d2f24788e765a9356acb
Tautvydas Kuklys <T.Kuklys@student.tudelft.nl>
2024-03-31 16:48:20 +0200

    Adding language property files

--- client/src/main/java/client/utils/LanguageSingleton.java
+++ client/src/main/java/client/utils/LanguageSingleton.java
@@ -3,13 +3,20 @@ package client.utils;
 
 import javafx.scene.image.Image;
 import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Optional;
 
 public class LanguageSingleton {
     private static final LanguageSingleton instance = new LanguageSingleton();
     private Pair<String, Image> language;
 
+    @Value("${client.language}")
+    private String start_language;
+
     public LanguageSingleton() {
-        this.language = FlagListCell.getLanguages().get(0);
+        Optional<Pair<String, Image>> current_language =  FlagListCell.getLanguages().stream().filter(x -> x.getKey().equals(start_language)).findFirst();
+        this.language = current_language.orElseGet(() -> FlagListCell.getLanguages().get(0));
     }
 
     public Pair<String, Image> getLanguage() {
