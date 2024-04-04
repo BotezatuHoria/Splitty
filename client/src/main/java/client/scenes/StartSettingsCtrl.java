8b3a1a77c10acaa9f17016916b848b15b3c060c0
Tom van Leest <t.p.p.vanleest@student.tudelft.nl>
2024-04-04 12:18:01 +0200

    importing of the language template is now possible.

--- client/src/main/java/client/scenes/StartSettingsCtrl.java
+++ client/src/main/java/client/scenes/StartSettingsCtrl.java
@@ -5,15 +5,18 @@ import client.utils.LanguageSingleton;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
-import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.checkerframework.checker.units.qual.A;
 
+import javax.sound.sampled.Line;
+import javax.swing.*;
+import java.io.*;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class StartSettingsCtrl {
     private final MainCtrl mainCtrl;
@@ -37,6 +40,10 @@ public class StartSettingsCtrl {
 
     @FXML
     private Button okButton;
+
+    @FXML
+    private Label infoLabel;
+
 
     /**
      * Constructor for the statistics controller.
@@ -91,5 +98,71 @@ public class StartSettingsCtrl {
         serverLabel.visibleProperty().set(true);
         changeButton.visibleProperty().set(true);
         okButton.visibleProperty().set(false);
+    }
+
+    /**
+     * makes info text about importing the template file appear.
+     */
+    public void showInfo(){
+        infoLabel.setText("Import a template which you can use to add a language. Sent this filled template to the moderators of the app, group53@gmail.com");
+    }
+
+    /**
+     * the info-text disappear again.
+     */
+    public void notShowInfo(){
+        infoLabel.setText("");
+    }
+
+    /**
+     * imports the template to a location the user can choose.
+     * @throws IOException When the file to download is not found.
+     */
+    public void importTemplate() throws IOException {
+        File file = new File("client/src/main/resources/messages_en.properties"); // needs to be edited to correct template!!!!
+        JFrame parent = new JFrame();
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setSelectedFile(new File("languageTemplate.properties"));
+
+        try{
+
+            int returnValue = fileChooser.showOpenDialog(parent);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File saveFile = fileChooser.getSelectedFile();
+                FileInputStream in = new FileInputStream(file);
+                FileOutputStream out = new FileOutputStream(saveFile);
+                try {
+
+                    int n;
+
+                    // read() function to read the
+                    // byte of data
+                    while ((n = in.read()) != -1) {
+                        // write() function to write
+                        // the byte of data
+                        out.write(n);
+                    }
+                } finally {
+                    if (in != null) {
+
+                        // close() function to close the
+                        // stream
+                        in.close();
+                    }
+                    // close() function to close
+                    // the stream
+                    if (out != null) {
+                        out.close();
+                    }
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                    alert.setContentText("Downloading of the template file succeeded!");
+                    alert.showAndWait();
+                }
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
