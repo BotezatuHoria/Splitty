6927f1f6ee75aad7c130e0df5a7820807ae433fb
Tom van Leest <t.p.p.vanleest@student.tudelft.nl>
2024-04-04 21:56:24 +0200

    testfile of starterpage

---
+++ client/src/test/java/client/scenes/StartSettingsCtrlTest.java
@@ -0,0 +1,48 @@
+package client.scenes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartSettingsCtrlTest {
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void setLanguageText() {
+    }
+
+    @Test
+    void adminLogin() {
+    }
+
+    @Test
+    void showStart() {
+    }
+
+    @Test
+    void changeServer() {
+    }
+
+    @Test
+    void confirmServer() {
+    }
+
+    @Test
+    void showInfo() {
+    }
+
+    @Test
+    void notShowInfo() {
+    }
+
+    @Test
+    void downloadTemplate() {
+    }
+
+    @Test
+    void responseServer() {
+    }
+}
