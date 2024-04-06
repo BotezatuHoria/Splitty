48e89950837bf5b63e8280d2127e05b0900c8cad
Ilia Ilinsky <I.S.Ilinsky-1@student.tudelft.nl>
2024-04-06 17:07:21 +0200

    Testing methods in  commons so that we have more than 80% coverage

--- commons/src/test/java/commons/PersonTest.java
+++ commons/src/test/java/commons/PersonTest.java
@@ -80,4 +80,62 @@ class PersonTest {
         test.addDept(3);
         assertEquals(3, test.getDebt());
     }
+
+    @Test
+    void getIdTest() {
+        Person test = new Person("test@email.com", "First", "Test",
+                "iban33");
+        assertEquals(test.getId(), 0);
+
+    }
+
+    @Test
+    void setEmailTest() {
+        Person test = new Person("test@email.com", "First", "Test",
+                "iban33");
+        test.setEmail("else@email.com");
+        assertEquals(test.getEmail(), "else@email.com");
+
+    }
+
+    @Test
+    void setFirstNameTest() {
+        Person test = new Person("test@email.com", "First", "Test",
+                "iban33");
+        test.setFirstName("Else");
+        assertEquals(test.getFirstName(), "Else");
+    }
+
+    @Test
+    void setLastNameTest() {
+        Person test = new Person("test@email.com", "First", "Test",
+                "iban33");
+        test.setLastName("Else");
+        assertEquals(test.getLastName(), "Else");
+    }
+
+    @Test
+    void setIbanTest() {
+        Person test = new Person("test@email.com", "First", "Test",
+                "iban33");
+        test.setIban("GU");
+        assertEquals(test.getIban(), "GU");
+    }
+
+    @Test
+    void setDebtTest() {
+        Person test = new Person("test@email.com", "First", "Test",
+                "iban33");
+        test.setDebt(3.0);
+        assertEquals(test.getDebt(), 3.0);
+    }
+
+    @Test
+    void toStringTest() {
+        Person test = new Person("test@email.com", "First", "Test",
+                "iban33");
+        assertEquals(test.toString(), "First Test");
+    }
+
+
 }
