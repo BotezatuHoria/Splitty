256c8c68e18640ff993de165ca34bd56039afb36
Horia Botezatu <h.a.botezatu@student.tudelft.nl>
2024-04-07 22:25:05 +0200

    Added some test for EventControllerTest

--- client/src/main/java/client/scenes/DebtOverviewPageCtrl.java
+++ client/src/main/java/client/scenes/DebtOverviewPageCtrl.java
@@ -4,7 +4,6 @@ import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import commons.Person;
 import javafx.application.Platform;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
