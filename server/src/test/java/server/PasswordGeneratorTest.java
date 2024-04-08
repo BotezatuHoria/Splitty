28346978ba50d997506081759eade06dc37c6cf1
Artur Iurasov <A.Iurasov@student.tudelft.nl>
2024-04-08 19:54:29 +0200

    Wrote tests for PasswordGenerator class

---
+++ server/src/test/java/server/PasswordGeneratorTest.java
@@ -0,0 +1,52 @@
+package server;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+
+public class PasswordGeneratorTest {
+
+  @Mock
+  private Logger logger;
+
+  @InjectMocks
+  private PasswordGenerator passwordGenerator;
+
+  @Captor
+  ArgumentCaptor<String> captor;
+
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.openMocks(this);
+  }
+/**
+   * Tests if the password is generated.
+   */
+  @Test
+  public void testGeneratePassword() {
+    passwordGenerator.generatePassword();
+    String password = PasswordGenerator.getPassword();
+    assertEquals(8, password.length());
+  }
+
+  /**
+   * Tests if the password is logged.
+
+   */
+  @Test
+  public void testGetPassword() {
+    passwordGenerator.generatePassword();
+    String password = PasswordGenerator.getPassword();
+    assertTrue(password.matches("[a-zA-Z0-9]{8}"));
+  }
+
+}
