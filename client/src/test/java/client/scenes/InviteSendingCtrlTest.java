ceaa2385723e6024712854d6fdb8e56cf6545d72
Tom van Leest <t.p.p.vanleest@student.tudelft.nl>
2024-04-14 18:18:08 +0200

    test file invite page

---
+++ client/src/test/java/client/scenes/InviteSendingCtrlTest.java
@@ -0,0 +1,48 @@
+package client.scenes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InviteSendingCtrlTest {
+
+    @Test
+    void sendInvite() {
+    }
+
+    @Test
+    void sendCheck() {
+    }
+
+    @Test
+    void setCheckLabel() {
+    }
+
+    @Test
+    void unSetCheckLabel() {
+    }
+
+    @Test
+    void setEventTitle() {
+    }
+
+    @Test
+    void setShareCode() {
+    }
+
+    @Test
+    void copyCode() {
+    }
+
+    @Test
+    void setLanguageText() {
+    }
+
+    @Test
+    void cancelGoBack() {
+    }
+
+    @Test
+    void initialize() {
+    }
+}
